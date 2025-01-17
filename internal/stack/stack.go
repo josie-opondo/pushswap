1055bbece986733a88aed6ec6458745b249cf0ac
josie-opondo <josephineopondo5@gmail.com>
2025-01-17 07:36:24 +0300

    add Pop method which removes and returns the top element of the stack.

--- internal/stack/stack.go
+++ internal/stack/stack.go
@@ -20,3 +20,13 @@ func NewStack(name string) *Stack {
 func (s *Stack) Push(val int) {
 	s.data = append([]int{val}, s.data...)
 }
+
+// Pop removes and returns the top element of the stack.
+func (s *Stack) Pop() (int, error) {
+	if len(s.data) == 0 {
+		return 0, errors.New("stack is empty")
+	}
+	val := s.data[0]
+	s.data = s.data[1:]
+	return val, nil
+}
