99112c3adf4a15f45810e2fc780e470f252c3d83
josie-opondo <josephineopondo5@gmail.com>
2025-01-17 07:35:36 +0300

    add Push method which adds an element to the top of the stack

--- internal/stack/stack.go
+++ internal/stack/stack.go
@@ -16,3 +16,7 @@ func NewStack(name string) *Stack {
 	return &Stack{data: []int{}, name: name}
 }
 
+// Push adds an element to the top of the stack.
+func (s *Stack) Push(val int) {
+	s.data = append([]int{val}, s.data...)
+}
