d477bcd0d453cc9840eab7e01885de6d4d3bc98e
josie-opondo <josephineopondo5@gmail.com>
2025-01-17 08:49:28 +0300

    fix: add additional functions to optimize and improve stacking logic

--- internal/stack/stack.go
+++ internal/stack/stack.go
@@ -2,7 +2,6 @@ package stack
 
 import (
 	"errors"
-	"fmt"
 )
 
 // Stack represents a stack of integers.
@@ -22,18 +21,45 @@ func NewStack(nums []int) *Stack {
 }
 
 // Push adds an element to the top of the stack.
-func (s *Stack) Push(val int) {
+func (s *Stack) PushFront(val int) {
 	s.data = append([]int{val}, s.data...)
 }
 
+func (s *Stack) PushBack(n int) {
+	s.data = append(s.data, n)
+}
+
 // Pop removes and returns the top element of the stack.
-func (s *Stack) Pop() (int, error) {
+func (s *Stack) Pop() int {
 	if len(s.data) == 0 {
-		return 0, errors.New("stack is empty")
+		return -1
 	}
-	val := s.data[len(s.data)-1]
+	res := s.data[0]
+	s.data = s.data[1:]
+	return res
+}
+
+func (s *Stack) PopBack() int {
+	if len(s.data) == 0 {
+		return -1
+	}
+	res := s.data[len(s.data)-1]
 	s.data = s.data[:len(s.data)-1]
-	return val, nil
+	return res
+}
+
+ //push the top first element of stack b to stack a
+ func pa(s []*Stack) {
+	if len(s[1].data) > 0 {
+		s[0].PushBack(s[1].PopBack())
+	}
+}
+
+ //push the top first element of stack a to stack b
+ func pb(s []*Stack) {
+	if len(s[0].data) > 0 {
+		s[1].PushBack(s[0].PopBack())
+	}
 }
 
 // Swap swaps the first two elements of the stack.
@@ -61,7 +87,120 @@ func (s *Stack) ReverseRotate() {
 	}
 }
 
-// Print displays the stack.
-func (s *Stack) Print() {
-	fmt.Printf("%s: %v\n", s.name, s.data)
+//swap first 2 elements of stack a
+func sa(s []*Stack) {
+	if len(s[0].data) > 1 {
+		v1 := s[0].PopBack()
+		v2 := s[0].PopBack()
+		s[0].PushBack(v1)
+		s[0].PushBack(v2)
+	}
+}
+
+ //swap first 2 elements of stack b
+func sb(s []*Stack) {
+	if len(s[1].data) > 1 {
+		v1 := s[1].PopBack()
+		v2 := s[1].PopBack()
+		s[1].PushBack(v1)
+		s[1].PushBack(v2)
+	}
+}
+
+ //execute sa and sb
+func ss(s []*Stack) {
+	sa(s)
+	sb(s)
+}
+
+ //rotate stack a (shift up all elements of stack a by 1, the first element becomes the last one)
+func ra(s []*Stack) {
+	if len(s[0].data) == 0 {
+		return
+	}
+	s[0].PushFront(s[0].PopBack())
+}
+
+ //rotate stack b
+func rb(s []*Stack) {
+	if len(s[1].data) == 0 {
+		return
+	}
+	s[1].PushFront(s[1].PopBack())
+}
+
+ //execute ra and rb
+func rr(s []*Stack) {
+	ra(s)
+	rb(s)
+}
+
+//reverse rotate a (shift down all elements of stack a by 1, the last element becomes the first one)
+func rra(s []*Stack) {
+	if len(s[0].data) == 0 {
+		return
+	}
+	s[0].PushBack(s[0].Pop())
+}
+
+ //reverse rotate b
+func rrb(s []*Stack) {
+	if len(s[1].data) == 0 {
+		return
+	}
+	s[1].PushBack(s[1].Pop())
+}
+
+ //execute rra and rrb
+func rrr(s []*Stack) {
+	rra(s)
+	rrb(s)
+}
+
+
+type fn func(stack []*Stack)
+
+var Cmd = map[string]fn{
+	"pa":  pa,
+	"pb":  pb,
+	"sa":  sa,
+	"sb":  sb,
+	"ss":  ss,
+	"ra":  ra,
+	"rb":  rb,
+	"rr":  rr,
+	"rra": rra,
+	"rrb": rrb,
+	"rrr": rrr,
+}
+
+func Operation(stack []*Stack, instr string) {
+	Cmd[instr](stack)
+}
+
+func RadixSort(data []int) string {
+	res := ""
+	stack := []*Stack{NewStack(data), NewStack(make([]int, 0, len(data)))}
+	maxNum := len(stack[0].data) - 1
+	maxBits := 0
+	for maxNum>>maxBits != 0 {
+		maxBits++
+	}
+	for i := 0; i < maxBits; i++ {
+		for j := 0; j < len(data); j++ {
+			num := stack[0].data[len(stack[0].data)-1]
+			if (num>>i)&1 == 1 {
+				Operation(stack, "ra")
+				res += "ra\n"
+			} else {
+				Operation(stack, "pb")
+				res += "pb\n"
+			}
+		}
+		for len(stack[1].data) != 0 {
+			Operation(stack, "pa")
+			res += "pa\n"
+		}
+	}
+	return res
 }
